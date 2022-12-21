7d58784fbb45f3b62ecb87691ceb167fd09cbba8
Teddy Muller <muller.teddy@icloud.com>
2022-12-21 13:34:39 +0100

    seed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @bookings = Booking.where(user: current_user)
+    @cars = Car.where(user: current_user)
   end
 end
